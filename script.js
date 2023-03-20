830c60dc510afac755a79e937c26e5e0d10cfbff
zobaer <zobaerislam62@gmail.com>
2023-03-20 17:59:21 +0600

    resetting the game and some code customization

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const player1El = document.querySelector('.player--1')
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+
 const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
-
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -16,16 +16,37 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// Define essential variable
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
+// Starting codition
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+
+// Initialize conditions setup
+init();
+
+
+// Player switching
 const switchPlayer = function () {
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
@@ -57,6 +78,8 @@ btnRoll.addEventListener('click', function() {
     }
 });
 
+
+// Hold button functionality
 btnHold.addEventListener('click', function() {
     if (playing) {
         // 1. Add current score to active players score;
@@ -64,7 +87,7 @@ btnHold.addEventListener('click', function() {
         document.getElementById('score--' + activePlayer).textContent = scores[activePlayer];
     
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 30) {
+        if (scores[activePlayer] >= 100) {
             // finish the game  
             playing = false;
             diceEl.classList.add('hidden');
@@ -76,5 +99,8 @@ btnHold.addEventListener('click', function() {
             switchPlayer();
         }
     }
+});
 
-})
+
+// Start new game
+btnNew.addEventListener('click', init);
